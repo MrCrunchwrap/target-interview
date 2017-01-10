2e9f12bbaa1159f3e84069602418742ecc75aa6c
Michael McDonald <mcdonald.michael.mn@gmail.com>
2017-01-10 02:07:49 -0600

    adding image carousel markup and styles

--- app/components/ProductView.js
+++ app/components/ProductView.js
@@ -2,8 +2,27 @@ import Marionette from 'backbone.marionette';
 import productItemViewTemplate from '../templates/productItemViewTemplate.hbs';
 
 export default Marionette.View.extend({
+  
   template: productItemViewTemplate,
 
-  initialize() {
-  },
+  onDomRefresh() {
+
+  	// initialize slick carousel
+  	this.$('.slider-for').slick({
+  		slidesToShow: 1,
+  		slidesToScroll: 1,
+  		arrows: false,
+  		fade: true,
+  		asNavFor: '.slider-nav'
+  	});
+
+  	this.$('.slider-nav').slick({
+  		slidesToShow: 3,
+  		slidesToScroll: 1,
+  		asNavFor: '.slider-for',
+  		dots: true,
+  		centerMode: true,
+  		focusOnSelect: true
+  	});
+  }
 });
