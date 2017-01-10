4c67a5311e2588bb282f583d8965371172a4bd6b
Michael McDonald <mcdonald.michael.mn@gmail.com>
2017-01-10 02:10:16 -0600

    adding more data to model

--- app/components/ProductViewModel.js
+++ app/components/ProductViewModel.js
@@ -2,11 +2,14 @@ import Backbone from 'backbone'
 
 export default Backbone.Model.extend({
   initialize(opts) {
+    // TODO: convert this to object hash
     this.set('productFeatures', opts.ItemDescription[0].features);
     this.set('primaryImageUrl', opts.Images[0].PrimaryImage[0].image);
     this.set('showAddToCart', this.showAddToCart(opts.purchasingChannelCode));
     this.set('showPickupInStore', this.showPickupInStore(opts.purchasingChannelCode));
-    this.set('priceData', opts.Offers[0].OfferPrice[0])
+    this.set('priceData', opts.Offers[0].OfferPrice[0]);
+    this.set('promos', opts.Promotions);
+    this.set('thumbnails', opts.Images[0].AlternateImages)
   },
 
   showAddToCart(purchasingChannelCode) {
